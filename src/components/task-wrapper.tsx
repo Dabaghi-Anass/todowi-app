eb9dcc388f7b7062f62b9b38104281aa7e12251c
Dabaghi-Anass <anass.debbaghi123@gmail.com>
2023-06-08 20:33:04 +0100

    building changes

--- src/components/task-wrapper.tsx
+++ src/components/task-wrapper.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import AppIcon from "./app-icon";
 import FormattedText from "./FormatedText";
 type Word = {
@@ -27,30 +27,77 @@ interface TasksProps{
   onDelete : (e: React.MouseEvent) => void;
   onColorChange : (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
-export default function TaskWrapper({ item, onDelete,onColorChange }: TasksProps) {
+export default function TaskWrapper({ item, onDelete, onColorChange }: TasksProps) {
+  const [selectedText, setSelectedText] = useState("");
+  const [itemClone, setItem] = useState<Task>(item);
 
+  const boldenText = () => {
+    let allText = item.content.split(" ").map(w => w.trim());
+    let words: Word[] = selectedText.split(' ').map(w => ({ word: w.trim(), index: allText.indexOf(w) }));
+    setItem(prev => ({
+      ...prev,
+      boldWords :[...prev.boldWords , ...words]
+    }))
+  }
+  const italicText = () => {
+    let allText = item.content.split(" ").map(w => w.trim());
+    let words: Word[] = selectedText.split(' ').map(w => ({ word: w.trim(), index: allText.indexOf(w) }));
+    setItem(prev => ({
+      ...prev,
+      italicWords :[...prev.italicWords , ...words]
+    }))
+  }
+  const underlineText = () => {
+    let allText = item.content.split(" ").map(w => w.trim());
+    let words: Word[] = selectedText.split(' ').map(w => ({ word: w.trim(), index: allText.indexOf(w) }));
+    setItem(prev => ({
+      ...prev,
+      underlinedWords :[...prev.underlinedWords , ...words]
+    }))
+  }
+  const updateHeader = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setItem(prev => ({
+      ...itemClone,
+      category: e.target.value
+    }))
+  }
+  const striketroughText = () => {
+    let allText = item.content.split(" ").map(w => w.trim());
+    let words: Word[] = selectedText.split(' ').map(w => ({ word: w.trim(), index: allText.indexOf(w) }));
+    setItem(prev => ({
+      ...prev,
+      lineCrossedWords :[...prev.lineCrossedWords , ...words]
+    }))
+  }
+  useLayoutEffect(() => {
+    document.addEventListener('mouseup', function() {
+      var selectedText = window.getSelection()?.toString();
+    if (selectedText?.length)
+      setSelectedText(prev => selectedText || "");
+  });
+
+  },[])
   return (
     <div className="task-wrapper" style={{
-      background: item.background,
+      background: itemClone.background,
     }}>
       <div className="task-wrapper-section head">
-        <span id="category">
-          {item.category}</span>
+        <input id="category" value={itemClone.category} onChange={updateHeader} />
         <span>
-          <AppIcon name={`PushPin${item.isPinned ? "" : "Outlined"}`}
+          <AppIcon name={`PushPin${itemClone.isPinned ? "" : "Outlined"}`}
           className="icon icon-pin"/>
           <AppIcon name="DeleteOutline" onClick={(e) => onDelete(e)} className="icon icon-delete" />
         </span>
     </div>
     <div className="task-wrapper-section content">
-    <FormattedText task={item} />
+    <FormattedText task={itemClone} />
     </div>
       <div className="task-wrapper-section footer">
         <div>
-          <AppIcon name="FormatBold" className="icon icon-bold"/>
-          <AppIcon name="FormatItalic" className="icon icon-italic"/>
-          <AppIcon name="FormatUnderlined" className="icon icon-underline"/>
-          <AppIcon name="FormatStrikethrough" className="icon icon-strike"/>
+          <AppIcon name="FormatBold" className="icon icon-bold" onClick={boldenText}/>
+          <AppIcon name="FormatItalic" className="icon icon-italic" onClick={italicText}/>
+          <AppIcon name="FormatUnderlined" className="icon icon-underline" onClick={underlineText}/>
+          <AppIcon name="FormatStrikethrough" className="icon icon-strike" onClick={striketroughText}/>
         </div>
         <div>
           <label>

2f781249ddf244e6650d429c260f945d8f22bc10
Dabaghi-Anass <anass.debbaghi123@gmail.com>
2023-06-28 14:24:15 +0100

    removed block add

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -21,20 +21,6 @@ export const HomePage = () => {
         </section>
       </header>
       <section className="description fullHeight">
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1245708667104363"
-          crossOrigin="anonymous"
-        ></script>
-        <ins
-          className="adsbygoogle"
-          style={{ display: "block" }}
-          data-ad-client="ca-pub-1245708667104363"
-          data-ad-slot="7903882774"
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-        ></ins>
-        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
         <p>
           For better productivité you need
           <br />
